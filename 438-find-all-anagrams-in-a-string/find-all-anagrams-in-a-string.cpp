d75203d5e53b815fc9ee15f23dfd4d17553c1f99
Kailashpharswan <kailash.pharswan02@gmail.com>
2026-08-19 14:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (63.68%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool allzero(vector<int>& counter){
+        for(int &i : counter){
+            if(i!=0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+public:
+    vector<int> findAnagrams(string s, string p) {
+       int n=s.size();
+       vector<int>counter(26,0);
+       for(int i=0;i<p.length();i++){
+        char ch=p[i];
+        counter[ch-'a']++;
+       } 
+       int i=0 , j=0;
+       vector<int> result ;
+       while(j<n){
+        counter[s[j]-'a']--;
+        if(j-i+1 == p.length()){
+            if(allzero(counter)){
+                result.push_back(i);
+            }
+            counter[s[i]-'a']++;
+            i++;
+        }
+        j++;
+       }
+       return result;
+
+    }
+};
