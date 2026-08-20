bb02e7a1739b0013fae26f0527627dccdf68b377
Kailashpharswan <kailash.pharswan02@gmail.com>
2026-08-20 14:58:29 +0530

    Time: 7 ms (39.48%) | Memory: 10.1 MB (45.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -6,14 +6,34 @@ public:
         if(n>m){
             return false;
         }
-        sort(s1.begin(),s1.end());
-        for(int i=0;i<=m-n;i++){
-            string substring =s2.substr(i,n);
-            sort(substring.begin(),substring.end());
-            if(s1==substring){
+        // sort(s1.begin(),s1.end());
+        // for(int i=0;i<=m-n;i++){
+        //     string substring =s2.substr(i,n);
+        //     sort(substring.begin(),substring.end());
+        //     if(s1==substring){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        /////now by sliding window
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
+        for(char &ch: s1){
+            s1_freq[ch-'a']++;
+        }
+        int r=0,l=0;
+        while(r<m){
+            s2_freq[s2[r]-'a']++;
+            if(r-l+1 > n){
+                s2_freq[s2[l]-'a']--;
+                l++;
+            }
+            if(s1_freq==s2_freq){
                 return true;
             }
+            r++;
         }
-        return false;
+         return false ;
+
     }
 };
