bb439ef7d2b2a5c121520a4b35442cb1550c6b6a
Kailashpharswan <kailash.pharswan02@gmail.com>
2026-08-21 23:22:17 +0530

    Time: 3 ms (78.26%) | Memory: 11.5 MB (43.53%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        unordered_map<char,int> mpp;
+        int count=0,r=0,l=0;
+        int minlen=INT_MAX;
+        int starting_index=-1;
+        for(int i=0;i<m;i++){
+            mpp[t[i]]++;
+        }
+        while(r<n){
+            if(mpp[s[r]] > 0){
+                count++;
+            }
+            mpp[s[r]]--;
+            while(count==m){
+                if(r-l+1<minlen){
+                    minlen=r-l+1;
+                    starting_index=l;
+                }
+                mpp[s[l]]++;
+                if(mpp[s[l]]>0){
+                    count--;
+                }
+                l++;
+            }
+            r++;
+        }
+       if(starting_index == -1) {
+            return "";
+        }
+
+        return s.substr(starting_index, minlen);
+    }
+};
